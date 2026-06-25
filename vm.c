6b3590422f193e5a26f394870899a5b49ea80057
Nathanael Garcia <nathanaelg2099@gmail.com>
2026-06-24 20:15:15 -0700

    Added LDI, ADD, NOT, AND

--- vm.c
+++ vm.c
@@ -1,8 +1,44 @@
+#include <stdio.h>
 #include <stdint.h>
+#include <signal.h>
+/* windows only */
+#include <Windows.h>
+#include <conio.h>  // _kbhit
+
+
+HANDLE hStdin = INVALID_HANDLE_VALUE;
+DWORD fdwMode, fdwOldMode;
+
+void disable_input_buffering()
+{
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &fdwOldMode); /* save old mode */
+    fdwMode = fdwOldMode
+            ^ ENABLE_ECHO_INPUT  /* no input echo */
+            ^ ENABLE_LINE_INPUT; /* return when one or
+                                    more characters are available */
+    SetConsoleMode(hStdin, fdwMode); /* set new mode */
+    FlushConsoleInputBuffer(hStdin); /* clear buffer */
+}
+
+void restore_input_buffering()
+{
+    SetConsoleMode(hStdin, fdwOldMode);
+}
+
+uint16_t check_key()
+{
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+}
+
+
+
+/* Memory */
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
 
-enum
+/* Registers */
+enum Registers
 {
     R_R0 = 0,
     R_R1,
@@ -18,7 +54,8 @@ enum
 };
 uint16_t reg[R_COUNT];
 
-enum
+/* Opcodes */
+enum Opcodes
 {
     OP_BR = 0,
     OP_ADD,
@@ -37,3 +74,190 @@ enum
     OP_LEA, //load effective address
     OP_TRAP //execute trap
 };
+
+/* Condition Flags*/
+enum condition_flags
+{
+    FL_POS = 1 << 0,
+    FL_ZRO = 1 << 1,
+    FL_NEG = 1 << 2,
+};
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
+
+
+
+
+
+
+
+
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2)
+    {
+        /* Show usage string*/
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("Failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+    @Setup
+
+    /* Only 1 condition flag should be set at any given time*/
+    reg[R_cond]=FL_ZRO;
+
+    //Starting PC is 0x3000
+    enum { PC_START = 0x3000};
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while(running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                /* destination register (DR) */
+                uint16_t r0 = (instr >> 9) & 0x7;
+                /* first operand (SR1) */
+                uint16_t r1 = (instr >> 6) & 0x7;
+                /* whether we are in immediate mode */
+                uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                if (imm_flag)
+                {
+                    uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                    reg[r0] = reg[r1] + imm5;
+                }
+                else
+                {
+                    uint16_t r2 = instr & 0x7;
+                    reg[r0] = reg[r1] + reg[r2];
+                }
+
+                update_flags(r0);
+                break;
+
+
+            case OP_AND:
+            /* Parsing for registers and immediate flag*/
+                uint16_t r0 = (instr >> 9) & 0x7;
+                uint16_t r1 = (instr >> 6) & 0x7;
+                uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                if (imm_flag)
+                {
+                    /* Extend the immediate and store the result of AND into r0*/
+                    uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                    reg[r0] = reg[r1] & imm5;
+                }
+                else
+                {
+                    /* Find the value at r2 and store result of AND into r0*/
+                    uint16_t r2 = instr & 0x7;
+                    reg[r0] = reg[r1] & reg[r2];
+                }
+                update_flags(r0);
+                break;
+
+
+            case OP_NOT:
+            /* Parse for registers r0 and r1*/
+                uint16_t r0 = (instr >> 9) & 0x7;
+                uint16_t r1 = (instr >> 6) & 0x7;
+
+                /* Perform bitwise NOT and store*/
+                reg[r0] = ~reg[r1];
+                update_flags(r0);
+                break;
+
+
+            case OP_BR:
+                @{BR}
+                break;
+            case OP_JMP:
+                @{JMP}
+                break;
+            case OP_JSR:
+                @{JSR}
+                break;
+            case OP_LD:
+                @{LD}
+                break;
+
+
+            case OP_LDI:
+                /* destination register (DR) */
+                uint16_t r0 = (instr >> 9) & 0x7;
+                /* PCoffset 9*/
+                uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+                /* add pc_offset to the current PC, look at that memory location to get the final address */
+                reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+                update_flags(r0);
+                break;
+
+
+            case OP_LDR:
+                @{LDR}
+                break;
+            case OP_LEA:
+                @{LEA}
+                break;
+            case OP_ST:
+                @{ST}
+                break;
+            case OP_STI:
+                @{STI}
+                break;
+            case OP_STR:
+                @{STR}
+                break;
+            case OP_TRAP:
+                @{TRAP}
+                break;
+            case OP_RES:
+                abort();
+            case OP_RTI:
+                abort();
+            default:
+                abort();
+                break;
+        }
+    }
+    @SHUTDOWN
+}
+
